c62b2d3b120b28c7eda7b8c795adc2a99017fd78
harris-ishaq <harris.ibrahim@gmail.com>
2022-02-10 01:20:01 +0800

    Update Report

--- resources/views/laporan/template-laporan-pdf.blade.php
+++ resources/views/laporan/template-laporan-pdf.blade.php
@@ -11,18 +11,37 @@
         table {
             width: 100%;
         }
-        table, th, td {
+        .table_data, th, td {
             border: 1px solid black;
             border-collapse: collapse;
         }
-	</style>
 
+        .kop {
+            border-collapse: collapse;
+        }
+        .noBorder {
+            border:none !important;
+        }
+	</style>
+    <table class="kop" style="border-collapse: collapse;" width="100%">
+        <tr>
+            <td class="noBorder" width="22" align="center"><img src="" width="60%"></td>
+            <td class="noBorder" width="55" align="center">
+                <h4>PEMERINTAH KABUPATEN KUTAI TIMUR<br>
+                DINAS PENDIDIKAN KECAMATAN MUARA BENGKAL<br>
+                SDN 001 MUARA BENGKAL<br>
+                Jl. Singa Raja RT.05 Desa Ngayau Kecamatan Muara Bengkal</h4>
+            </td>
+            <td class="noBorder" width="23" align="center"><img src="" width="100%"></td>
+        </tr>
+    </table>
+
+    <hr>
     <center>
-        <h2 style="margin-bottom:0px;">Laporan Perpustakaan</h2><br>
+        <h4 style="margin-bottom:0px;">Laporan Perpustakaan</h4><br>
         <p style="margin-top:0px;">Bulan {{ $bulan_awal }} - {{ $bulan_akhir }} {{ $year }}<br> SDN 001 Muara Bengkal</p>
     </center>
-    <hr>
-	<table border="1" class="table">
+	<table border="1" class="table_data">
         <thead>
             <tr>
                 <th>#</th>
@@ -30,7 +49,7 @@
 				<th class="text-center">Nama</th>
 				<th class="text-center">Judul Buku</th>
 				<th class="text-center">Tanggal Pinjam</th>
-                <th class="text-center">Batas Kembali</th>
+                <th class="text-center">Tanggal Kembali</th>
                 <th class="text-center">Denda</th>
 			</tr>
         </thead>
@@ -43,12 +62,12 @@
                     <td style="text-align:center">{{ $data->studentInformation->name }}</td>
                     <td style="text-align:center">{{ $data->bookInformation->title }}</td>
                     <td style="text-align:center">{{ $data->date_start->format('d-m-Y') }}</td>
-                    <td style="text-align:center">{{ $data->date_return->format('d-m-Y') }}</td>
+                    <td style="text-align:center">{{ $data->date_returned->format('d-m-Y') }}</td>
                     <td style="text-align:center">
                         @if ($data->date_returned > $data->date_return)
-                            Ya
+                            20.000
                         @else
-                            Tidak
+                            -
                         @endif
                     </td>
                 </tr>
@@ -63,6 +82,37 @@
                 <td style="text-align:center">{{ number_format(round($denda),0,",",".") }}</td>
             </tr>
         </tbody>
+    </table>
+    <br>
+    <br>
+    <br>
+    <br>
+    <table style="border-collapse: collapse;" width="100%">
+        <tr>
+            <td class="noBorder" width="80" align="center"></td>
+            <td class="noBorder" width="20" style="text-align:center">Muara Bengkal, 4 Januari 2022</td>
+        </tr>
+        <tr>
+            <td class="noBorder" width="80" align="center"></td>
+
+            <td class="noBorder" width="20" align="center">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="noBorder" width="80" align="center"></td>
+            <td class="noBorder" width="20" align="center">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="noBorder" width="80" align="center"></td>
+            <td class="noBorder" width="20" align="center">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="noBorder" width="80" align="center"></td>
+            <td class="noBorder" style="border-bottom: 1px solid black;" width="20" align="center"><b>SYAFRAN, S. Pd</b></td>
+        </tr>
+        <tr>
+            <td class="noBorder" width="80" align="center"></td>
+            <td class="noBorder" width="20" align="center">NIP. 19690514 200105 1 001</td>
+        </tr>
     </table>
 </body>
 </html>
